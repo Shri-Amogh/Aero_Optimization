43eb9f4f2bf2d400deef472f6857add41c4bb17c
Shri Amogh <shri.amogh@gmail.com>
2025-04-29 14:40:54 -0400

    Math Fix

--- Fortran Math/solver.f90
+++ Fortran Math/solver.f90
@@ -10,11 +10,9 @@ program solver
     real(dp) :: total_force(3), drag, lift
     real(dp) :: cp
 
-    ! Set freestream conditions
     U_inf = (/ 1.0_dp, 0.0_dp, 0.0_dp /)
     rho = 1.0_dp
 
-    ! Read mesh
     call read_mesh('data/vertices.dat', 'data/faces.dat', vertices, faces)
 
     n_vertices = size(vertices, 1)
@@ -23,13 +21,13 @@ program solver
     allocate(normals(n_faces, 3))
     allocate(areas(n_faces))
 
-    ! Compute panel normals and areas
+
     call compute_normals_areas(vertices, faces, normals, areas)
 
-    ! Initialize total force
+
     total_force = 0.0_dp
 
-    ! Loop over faces
+
     do i = 1, n_faces
         if (areas(i) > 1.0e-12_dp) then
             cp = -2.0_dp * dot_product(normals(i,:), U_inf)
@@ -37,20 +35,21 @@ program solver
         end if
     end do
 
-    ! Extract drag and lift components
-    drag = -total_force(1)  ! Drag along +X
-    lift = total_force(2)   ! Lift along +Y
 
-    ! Handle NaN or Inf safety
+    drag = -total_force(1) 
+    lift = total_force(2) 
+
+
     if (.not.(isfinite(drag))) drag = 0.0_dp
     if (.not.(isfinite(lift))) lift = 0.0_dp
 
-    ! Write output
+
     open(unit=10, file='data/forces.dat', status='replace', action='write')
     write(10,'(2F12.6)') drag, lift
     close(10)
 
-    ! Clean up
+
+    
     deallocate(vertices, faces, normals, areas)
 
 contains
